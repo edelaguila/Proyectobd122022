60d819a139b082d667534278178fd081c6924424
Byron Barrientos <byronivan2405@gmail.com>
2022-08-27 21:32:26 -0600

    rama de prueba

--- src/main/java/seguridad/modelo/clsConexion.java
+++ src/main/java/seguridad/modelo/clsConexion.java
@@ -3,9 +3,9 @@ package seguridad.modelo;
 import java.sql.*;
 
 public class clsConexion {
-    private static final String JDBC_URL = "jdbc:mysql://localhost/bdproyectop312022?useSSL=false&serverTimezone=UTC";
-    private static final String JDBC_USER = "root";
-    private static final String JDBC_PASS = "admin123";
+    private static final String JDBC_URL = "jdbc:mysql://localhost/proyectobd122022?useSSL=false&serverTimezone=UTC";
+    private static final String JDBC_USER = "Byron";
+    private static final String JDBC_PASS = "";
    
     
     public static Connection getConnection() throws SQLException {

a761f36826158fa0169e021377c5ef2171f974aa
ByronBH <98499307+ByronBH@users.noreply.github.com>
2022-09-07 11:19:45 -0600

    Conexion
    Arreglo de conexion

--- src/main/java/seguridad/modelo/clsConexion.java
+++ src/main/java/seguridad/modelo/clsConexion.java
@@ -3,7 +3,12 @@ package seguridad.modelo;
 import java.sql.*;
 
 public class clsConexion {
-     
+//    private static final String JDBC_URL = "jdbc:mysql://localhost/proyectob222022?'useSSL=false&serverTimezone=UTC";
+    private static final String JDBC_URL = "jdbc:mysql://localhost/proyectobd122022?useSSL=false&serverTimezone=UTC";    
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASS = "picodediamante777";
+    
+   
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
     }

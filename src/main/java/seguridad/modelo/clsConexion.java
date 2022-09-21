b313ccafac94d8ad565517c94842a3048f4f9d78
carlos enrique guzman cabrera <cguzmanc10@miumg.edu.gt>
2022-09-21 12:16:24 -0600

    todo listo

--- src/main/java/seguridad/modelo/clsConexion.java
+++ src/main/java/seguridad/modelo/clsConexion.java
@@ -3,10 +3,10 @@ package seguridad.modelo;
 import java.sql.*;
 
 public class clsConexion {
-   private static final String JDBC_URL = "jdbc:mysql://localhost/proyectob222022?'useSSL=false&serverTimezone=UTC";
-  //  private static final String JDBC_URL = "jdbc:mysql://192.168.0.21/proyectobd122022?useSSL=false&serverTimezone=UTC";    
-    private static final String JDBC_USER = "root";
-    private static final String JDBC_PASS = "";
+   //private static final String JDBC_URL = "jdbc:mysql://localhost/proyectob222022?'useSSL=false&serverTimezone=UTC";
+    private static final String JDBC_URL = "jdbc:mysql://192.168.1.201/proyectobd122022?useSSL=false&serverTimezone=UTC";    
+    private static final String JDBC_USER = "remoto";
+    private static final String JDBC_PASS = "123456";
     
    
     public static Connection getConnection() throws SQLException {

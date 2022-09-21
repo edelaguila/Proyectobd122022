13724e5c8fa8d4855219f120945dc38cace93715
carlos enrique guzman cabrera <cguzmanc10@miumg.edu.gt>
2022-09-21 13:13:12 -0600

    funciona

--- src/main/java/seguridad/modelo/clsConexion.java
+++ src/main/java/seguridad/modelo/clsConexion.java
@@ -4,9 +4,9 @@ import java.sql.*;
 
 public class clsConexion {
    //private static final String JDBC_URL = "jdbc:mysql://localhost/proyectob222022?'useSSL=false&serverTimezone=UTC";
-    private static final String JDBC_URL = "jdbc:mysql://192.168.1.201/proyectobd122022?useSSL=false&serverTimezone=UTC";    
+    private static final String JDBC_URL = "jdbc:mysql://192.168.0.17/proyectobd122022?useSSL=false&serverTimezone=UTC";    
     private static final String JDBC_USER = "remoto";
-    private static final String JDBC_PASS = "123456";
+    private static final String JDBC_PASS = "remoto";
     
    
     public static Connection getConnection() throws SQLException {

c49554fa58e82f307ea85850270ee7deb1f44d20
DanielRoblero1 <eroblerosciumg.edu.gt>
2022-11-02 21:11:37 -0600

    Agregacion de menu procesos ventas

--- src/main/java/seguridad/modelo/clsConexion.java
+++ src/main/java/seguridad/modelo/clsConexion.java
@@ -10,8 +10,8 @@ public class clsConexion {
     private static final String JDBC_URL = "jdbc:mysql://localhost/proyectobd122022?useSSL=false&serverTimezone=UTC";    
 
 
-    private static final String JDBC_USER = "remoto";
-    private static final String JDBC_PASS = "123456";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASS = "123456789";
    
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);

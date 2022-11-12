0225758ead4bfc0fb518f77e83fd3324ad487d30
ByronBH <byronivan2405@hotmail.com>
2022-11-11 22:46:35 -0600

    se agrega advertencia

--- scripts/script Movimiento.sql
+++ scripts/script Movimiento.sql
@@ -1,2 +1,5 @@
+ #ADVERTENCIA
+ #cabe resaltar que este script puede hacer que el proyecto no funcione
+ #no se recomienda correrlo!!!!!
 CREATE TRIGGER `Ingreso` AFTER INSERT ON `tbl_movimientode`
  FOR EACH ROW insert into tbl_bitacora (bitfecha, bitaccion, usuid, aplid, bitip, bitnombrepc) VALUES (CURRENT_DATE(), "insert", 4, 7, "192.1.100.1",  CURRENT_USER())
